b4b128b78915ecd6320fea7d25eb79f16b43a42a
Camilo Sanchez <111465555+LuxWise@users.noreply.github.com>
2024-10-28 13:26:41 -0500

    add more CORS_ALLOWED_ORIGINS

--- motionapi/settings.py
+++ motionapi/settings.py
@@ -93,21 +93,14 @@ WSGI_APPLICATION = 'motionapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('EXTERNAL_DATABASE_URL'),
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
+
+DATABASES = {
+    'default': dj_database_url.config(
+    default=os.getenv('EXTERNAL_DATABASE_URL'),
+    conn_max_age=600,
+    ssl_require=True,
+    )
+}
 
 
 
